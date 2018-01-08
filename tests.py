27e1345612fecc8a5e0975d727b904b3aed17a4e
RoySegall <roy.segall@gizra.com>
2018-01-08 21:03:07 +0200

    fix.

--- tests.py
+++ tests.py
@@ -19,5 +19,5 @@ def test_http_request():
     assert response.status_code == 405
 
     response = client.post('http://localhost/upload')
-    assert response.status_code == 309
+    assert response.status_code == 200
     assert response.json() == {'message': 'Not supported for now but will be :).'}
