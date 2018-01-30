20fd093f61d5587ffbc33b527c73714fe17cf074
Daniel Piankov <dpiankov@gmail.com>
2018-01-30 02:11:09 +0200

    created data structure

---
+++ ReportProcessor/BismarckReport.py
@@ -0,0 +1,62 @@
+import pandas as pd
+
+
+class BismarckReport(object):
+    """
+    Data class representing single report file to be validated with bismarck reports validator.
+    """
+    data = {
+        'metadata': {
+            'file_name': '',
+            'managing_body': '',
+            'year': '',
+            'month': '',
+            'quarter': '',
+            'file_level_results': [
+                {
+                    'result': '',
+                    'msg': ''
+                },
+            ],
+        },
+        'sheets': [
+            {
+                'sheet_name': '',
+                'sheet_code': '',
+                'rows_to_skip': '',
+                'number_of_columns': '',
+                'source_data': pd.DataFrame,
+                'sheet_level_results': [
+                    {
+                        'result': '',
+                        'msg': ''
+                    },
+                ],
+                'validation_results': [
+                    {
+                        'column_name': '',
+                        'column_code': '',
+                        'column_level_results': [
+                            {
+                                'result': '',
+                                'msg': ''
+                            },
+                        ],
+                        'rows': [
+                            {
+                                'row_number': '',
+                                'context': '',
+                                'number_of_validations': '',
+                                'row_level_results': [
+                                    {
+                                        'result': '',
+                                        'msg': ''
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    }
