7e11f72e3ef9a6bdb877885e8fb616672a55377d
RoySegall <roy.segall@gizra.com>
2018-02-05 20:53:03 +0200

    Adding base functions.

--- Rosetta.py
+++ Rosetta.py
@@ -1,29 +1,54 @@
 import yaml
-import sys
+import os
 
 
 class Rosetta(object):
 
-    def wrapper_file(self, folder):
+    # The folder of the yml files.
+    folder = ''
+
+    # Holds the list of contexts.
+    contexts = []
+
+    # Holds the list of contexts.
+    rosetta = {}
+
+    def __init__(self, folder, contexts=None):
+        if contexts is None:
+            contexts = ['in_israel', 'not_in_israel']
+
+        self.folder = folder
+        self.contexts = contexts
+
+    def get_constraints(self):
         """
         Specifying the file which holds the sub files.
 
-        :param folder:
+        :return:
+            All the constraints.
+        """
+
+        for filename in os.listdir(self.folder):
+            if not filename.endswith(".yml"):
+                continue
+
+            stream = open(self.folder + "/" + filename)
+            yml_file = yaml.load(stream)
+
+            self.rosetta[filename.split(".")[0]] = yml_file['fields']
+
+        return self.rosetta
+
+    def get_tab(self, tab):
+        """
+        Get info for tab.
+
+        :param tab:
+            The tab. i.e cache.
 
         :return:
         """
+        if self.rosetta == {}:
+            self.get_constraints()
 
-        # Open the file.
-        items = open(folder)
-
-        # Iterate over the files.
-        rosetta = []
-
-        for item in items:
-            stream = open(folder + "/" + item, "r")
-            content = yaml.load(stream)
-
-            # Todo: strip out data we don't need.
-            rosetta.append(content)
-
-        return rosetta
+        return self.rosetta[tab]
---
+++ TestsRosetta.py
@@ -0,0 +1,32 @@
+from Rosetta import Rosetta
+import os
+
+
+class TestsRosetta(object):
+
+    def _get_rosetta(self):
+        """
+        Helper function to get the rosetta stone.
+
+        :return:
+        """
+        return Rosetta(os.getcwd() + "/validations_templates")
+
+    def test_list_of_files(self):
+        """
+        Testing the rosetta return the list of files.
+
+        :return:
+        """
+        constraints = self._get_rosetta().get_constraints()
+        assert ['cash'] == list(constraints.keys())
+
+    def test_contexts(self):
+        """
+        Testing contexts issue methods.
+
+        :return:
+        """
+        tab = self._get_rosetta().get_tab('cash')
+        assert 'instrument_id' in tab.keys()
+        assert list(tab['instrument_id'].keys()) == self._get_rosetta().contexts
