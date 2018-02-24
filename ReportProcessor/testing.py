4511f4dc52e9f297ce4ab6efbac0148526bb066d
RoySegall <roy.segall@gizra.com>
2018-02-24 12:03:28 +0200

    Pushing code.

--- ReportProcessor/testing.py
+++ ReportProcessor/testing.py
@@ -4,6 +4,6 @@ import os
 
 from ReportProcessor.NewBismarkReport import NewBismarkReport
 
-pandas_excel = pd.ExcelFile(os.getcwd() + '/tests/')
+pandas_excel = pd.ExcelFile(os.getcwd() + '/tests/513026484_gsum_0317.xlsx')
 new_report = NewBismarkReport(pandas_excel)
 
