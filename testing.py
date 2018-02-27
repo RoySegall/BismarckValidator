58401619b7b177acff214502b9b74a430ecc0589
danielp16 <dpiankov@gmail.com>
2018-02-27 02:04:54 +0200

    added xls processor logic

--- testing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-
-from ReportProcessor.BismarkReport import BismarkReport
-
-pandas_excel = pd.ExcelFile(os.getcwd() + '/ReportProcessor/513026484_gsum_0317.xlsx')
-new_report = BismarkReport(pandas_excel).process_file()
-
