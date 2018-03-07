237d11e9e7d84151b73ba8fd4060d0924beeb21b
danielp16 <dpiankov@gmail.com>
2018-03-08 00:58:53 +0200

    rosetta rewrite 1

--- report_processor/bismarck_report.py
+++ report_processor/bismarck_report.py
@@ -4,79 +4,6 @@ from rosetta.rosetta import Rosetta
 
 class BismarckReport(object):
 
-    # Hold the list of the sheets and their code.
-    instrument_dict = {
-        'מזומנים': 'CASH',
-        'תעודות התחייבות ממשלתיות': 'GDC',
-        'תעודות חוב מסחריות': 'CDC',
-        'אג"ח קונצרני': 'CB',
-        'מניות': 'STOCK',
-        'תעודות סל': 'ETF',
-        'קרנות נאמנות': 'MF',
-        'כתבי אופציה': 'WARRANTS',
-        'אופציות': 'OPTIONS',
-        'חוזים עתידיים': 'FC',
-        'מוצרים מובנים': 'SP',
-        'לא סחיר - תעודות התחייבות ממשלתי': 'GDC',
-        'לא סחיר - תעודות התחייבות ממשלת': 'GDC',
-        'לא סחיר - תעודות חוב מסחריות': 'CDC',
-        'לא סחיר - אג"ח קונצרני': 'CB',
-        'לא סחיר - מניות': 'STOCK',
-        'לא סחיר - קרנות השקעה': 'IF',
-        'לא סחיר - כתבי אופציה': 'WARRANTS',
-        'לא סחיר - אופציות': 'OPTIONS',
-        'לא סחיר - חוזים עתידיים': 'FC',
-        'לא סחיר - מוצרים מובנים': 'SP',
-        'הלוואות': 'LOANS',
-        'פקדונות מעל 3 חודשים': 'DOTM',
-        'זכויות מקרקעין': 'LR',
-        'השקעות אחרות': 'OI',
-        'השקעה בחברות מוחזקות': 'IC',
-        'יתרת התחייבות להשקעה': 'ICB',
-        'עלות מותאמת אג״ח קונצרני סחיר': 'CBAC',
-        'עלות מותאמת אג״ח קונצרני לא סחיר': 'CBAC',
-        'עלות מותאמת מסגרות אשראי ללווים': 'BCAC',
-    }
-
-    # Keep the list of fields and their english representation.
-    field_list = {
-        'מספר ני"ע': 'instrument_id',
-        'מספר מנפיק': 'issuer_id',
-        'דירוג': 'rating',
-        'שם מדרג': 'rating_Agency',
-        'סוג מטבע': 'currency',
-        'שיעור ריבית': 'intrest_Rate',
-        'תשואה לפידיון': 'yield',
-        'שווי שוק': 'market_Cap',
-        'שעור מנכסי אפיק ההשקעה': 'rate_of_investment_channel',
-        'שעור מסך נכסי השקעה': 'rate_of_Fund',
-        'זירת מסחר': 'trading_floor',
-        'תאריך רכישה': 'date_of_purchase',
-        'מח"מ': 'average_of_Duration',
-        'ערך נקוב': 'par_value',
-        'שער': 'rate',
-        'שעור מערך נקוב מונפק': 'rate_of_ipo',
-        'ספק המידע': 'informer',
-        'שווי הוגן': 'fair_value',
-        'ענף מסחר': 'activity_industry',
-        'תאריך שערוך אחרון': 'date_of_revaluation',
-        'אופי הנכס': 'type_of_asset',
-        'שעור תשואה במהלך התקופה': 'tmp_name',
-        'סכום ההתחייבות': 'liabilities',
-        'תאריך סיום ההתחייבות': 'expiry_date_of_liabilities',
-        'שער ריבית': 'rate',
-        'ריבית אפקטיבית': 'effective_rate',
-        'עלות מתואמת': 'coordinated_cost',
-        'נכס הבסיס': 'underlying_asset',
-        'קונסורציום כן/לא': 'consortium',
-        'שיעור ריבית ממוצע': 'average rate',
-        'שווי מוערך': 'par_value',
-        'כתובת הנכס': 'instrument_address',
-        'פדיון/ ריבית לקבל': 'revenue_interest_receivable',
-        'שם המנפיק/שם נייר ערך': 'issuer_name',
-        'תנאי ושיעור ריבית': 'condition_and_interest_rate',
-    }
-
     general_errors = {}
 
     # errors_output = {
@@ -107,17 +34,23 @@ class BismarckReport(object):
                 continue
             for index, row_value in enumerate(sheet[column]):
                 row_name = str(sheet.index[index])
-                self.process_cell(column, index, row_name, row_value, context[index])
 
-    def process_cell(self, column, index, row_name, row_value, context):
-        if context:
-            rosetta_result = self.check_rosseta(column, index, row_name, row_value, context)
+                self.process_cell(sheet_name=sheet_name,
+                                  column=column,
+                                  index=index,
+                                  row_name=row_name,
+                                  row_value=row_value,
+                                  context=context[index])
+
+    def process_cell(self, **kwargs):
+        if kwargs.get('context'):
+            rosetta_result = self.check_rosseta(**kwargs)
+            # TODO - aggregate validation results
             if rosetta_result:
                 self.errors_output.append(rosetta_result)
 
     def check_rosseta(self, *args, **kwargs):
-        ros = Rosetta(None)
-        return ros.validate_object_test(*args, **kwargs)
+        return Rosetta().validate_object_test(*args, **kwargs)
 
     def get_sheet_context(self, sheet):
         # find context column
--- rosetta/rosetta.py
+++ rosetta/rosetta.py
@@ -2,12 +2,22 @@ import yaml
 import os
 
 from .validations import Validations
+from .rosetta_config import RosettaConfig
 
 
-class Rosetta(object):
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Rosetta(object, metaclass=Singleton):
 
     # The folder of the yml files.
-    folder = ''
+    folder = os.path.join(os.path.dirname(__file__), 'validation_templates')
 
     # Holds the list of contexts.
     contexts = []
@@ -15,41 +25,67 @@ class Rosetta(object):
     # Holds the list of contexts.
     rosetta = {}
 
-    def __init__(self, folder, contexts=None):
-        if contexts is None:
+    # Keep the list of fields and their english representation.
+    filds_list = RosettaConfig.FIELDS_LIST
+
+    # Hold the list of the sheets and their code.
+    instrument_dict = RosettaConfig.INSTRUMENT_DICT
+
+    # constraints dict
+    constraints_dict = {}
+
+    def __init__(self, folder=None, contexts=None):
+        if not contexts:
             contexts = ['in_israel', 'not_in_israel']
         if folder:
             self.folder = folder
-
-    def validate_object_test(self, *args, **kwargs):
-        if args:
-            print('{}|{}|{}|{}|{}'.format(args[0], args[1], args[2], args[3], args[4]))
-        return 'rosetta passed'
+        self.constraints_dict = self.get_constraints()
 
     def get_constraints(self):
         """
-        Specifying the file which holds the sub files.
-
+        Load sheets validations Specs.
         :return:
-            All the constraints.
         """
-
         for filename in os.listdir(self.folder):
-            if not filename.endswith(".yml"):
+            if not filename.endswith('.yml'):
                 continue
-
-            stream = open(self.folder + "/" + filename)
-            yml_file = yaml.load(stream)
-
-            self.rosetta[filename.split(".")[0]] = yml_file['fields']
+            with open(os.path.join(self.folder, filename), 'rt') as stream:
+                yml_file = yaml.load(stream)
+            self.rosetta[filename.split('.')[0]] = yml_file['fields']
 
         return self.rosetta
 
-    def get_tab(self, tab):
+    def validate_object_test(self, *args, **kwargs):
+        """
+        helper function during development.
+        generates input data for rosetta tests
+        :param **sheet_name = sheet_name,
+        :param **column = column,
+        :param **index = index,
+        :param **row_name = row_name,
+        :param **row_value = row_value,
+        :param **context = context[index]
+        :return:
+        """
+        with open('pre_rosetta.json', 'a', encoding='utf-8') as the_file:
+            the_file.write(
+                '{"sheet_name": "%s", "column": "%s", "index": %s,"row_name": "%s",'
+                ' "row_value": "%s", "context": "%s"},\n' % (
+                    str(kwargs.get('sheet_name')).replace("'", "\\'").replace('"', '\\"'),
+                    str(kwargs.get('column')).replace("'", "\\'").replace('"', '\\"'),
+                    kwargs.get('index'),
+                    str(kwargs.get('row_name')).replace("'", "\\'").replace('"', '\\"'),
+                    str(kwargs.get('row_value')).replace("'", "\\'").replace('"', '\\"'),
+                    str(kwargs.get('context')).replace("'", "\\'").replace('"', '\\"')
+                ))
+
+        return
+
+    def get_sheet(self, sheet):
         """
         Get info for tab.
 
-        :param tab:
+        :param sheet:
             The tab. i.e cache.
 
         :return:
@@ -57,7 +93,7 @@ class Rosetta(object):
         if self.rosetta == {}:
             self.get_constraints()
 
-        return self.rosetta[tab]
+        return self.rosetta[sheet]
 
     def validate_object(self, tab, _object):
         """
